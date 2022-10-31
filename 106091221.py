c7f34b9c4d7698727ac7ec01a08b7e1e89141bf9
Sophia <sophias94171@gmail.com>
2022-10-31 17:44:10 +0800

    test

--- 106091221.py
+++ 106091221.py
@@ -1,6 +1,9 @@
 from gc import callbacks
 import streamlit as st
+from annotated_text import annotated_text
 import Spellchecker as sp
+
+
 
 def callback1():
     st.session_state.text = st.session_state.selectbox
@@ -8,6 +11,8 @@ def callback2():
     st.session_state.text = st.session_state.text
 
 st.title("Spellchecker")
+st.subheader('by Sophia Yang')
+
 with st.sidebar:
     checkbox = st.checkbox("Show original word", key = 'checkbox')
 
@@ -15,11 +20,19 @@ selectbox = st.selectbox("Choose a word or ...", ['apple','lamon','speling','hap
 
 text = st.text_input('Type your own!!',value='', on_change = callback2, key = 'text')
 
-
-if text:
-    st.write(sp.correction(text))
-
 if checkbox:
     st.write('Original word: ', text)
 
-st.write(st.session_state)
+if text:
+    if sp.correction(text) == text:
+        annotated_text(
+            (sp.correction(text) + ' is the correct spelling!', '', '#afa')
+        )
+    else:
+         annotated_text(
+            ( 'Correction: ' + sp.correction(text), '', '#faa')
+        )
+
+
+# st.session_state
+
