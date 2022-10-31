eb89f32c03a6a5dfd41495312d3330b5ef031fc9
Sophia <sophias94171@gmail.com>
2022-10-31 14:00:46 +0800

    test

--- 106091221.py
+++ 106091221.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.checkbox("Show original word")
+st.title("Spellchecker")
+with st.sidebar:
+    st.checkbox("Show original word")
 st.selectbox("Choose a word or ...", ['apple','lamon','speling','hapy','language','greay','sussuss'])
 st.text_input("Type your own!!")
-st.title("Spellchecker")
